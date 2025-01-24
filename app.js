af93bcfd0e2138db7563c8037267f61f0cac826f
Eliseu Brito <eliseubrito@protonmail.com>
2025-01-24 11:08:32 -0300

    adicionado função para listagem de amigos

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let amigos = []
+let amigos = [];
 
 function adicionarAmigo(){
     let nome = document.getElementById("amigo");
     if(nome.value == ""){
-        alert("Por favor, insira um nome.")
+        alert("Por favor, insira um nome.");
     }
-    amigos.push(nome.value)
+    amigos.push(nome.value);
     nome.value = "";
+    listaDeAmigos();
+}
+
+function listaDeAmigos(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    if(amigos.length > 0){
+        amigos.forEach(element => {
+            let item = document.createElement("li");
+            item.textContent = element;
+            lista.appendChild(item);
+        });
+    }
 }
