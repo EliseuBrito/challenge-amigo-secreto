e3d1b21065381dbc560bc7fca87d4d4e8adc0f69
Eliseu Brito <eliseubrito@protonmail.com>
2025-01-24 10:42:29 -0300

    adicionado função para adicionar amigo

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = []
+
+function adicionarAmigo(){
+    let nome = document.getElementById("amigo");
+    if(nome.value == ""){
+        alert("Por favor, insira um nome.")
+    }
+    amigos.push(nome.value)
+    nome.value = "";
+}
